2ab402f9d95009bada5ce3c82b33b8f40186cae6
UTakuto <taku.0307.ue@icloud.com>
2024-09-26 15:25:18 +0900

    Add home page link post

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 const Home = () => {
     <div>
         <h1>Welcome to Next.js with TypeScript</h1>
-        <Link href="/about">
+        {/* about page link */}
+        {/* <Link href="/about">
             <a>go to about page</a>
-        </Link>
+        </Link> */}
+
+        {/* posts page link */}
+        <Link href="/posts/1"></Link>
     </div>;
 };
 
